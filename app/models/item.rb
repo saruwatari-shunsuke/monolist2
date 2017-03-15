512fd720ae89f0317f48cb3772fb2503ba4011d7
Shunsuke Saruwatari <ssaruwatari@agentgate.jp>
2017-03-15 04:09:38 +0000

    rakuten_items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ActiveRecord::Base
   has_many :ownerships  , foreign_key: "item_id" , dependent: :destroy
   has_many :users , through: :ownerships
+  validates :q, allow_blank: true, length: { minimum: 2, maximum: 20 }
 end
